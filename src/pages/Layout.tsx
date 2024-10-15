9da0b361f4351620cbd11c8eceac57a5307c665e
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-15 23:42:57 +0330

    Fix layout style

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,7 +3,7 @@ import Sidebar from "../components/Sidebar";
 
 const Layout = () => {
   return (
-    <div className="min-h-screen w-full flex">
+    <div className="min-h-screen w-full flex bg-gray-100 dark:bg-gray-800">
       <Sidebar />
       <main className="flex-grow w-full h-full max-sm:pl-20">
         <Outlet />
--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -9,7 +9,7 @@ const TasksPage = () => {
   useSelectCategory(categoryId ?? null);
 
   return (
-    <div className="min-h-screen w-full md:px-5 md:py-4 bg-gray-100 dark:bg-gray-800">
+    <div className="min-h-screen w-full md:px-5 md:py-4">
       <div className="container mx-auto flex flex-col gap-2 h-full w-full p-4">
         <TaskFilter />
         <TaskList />
