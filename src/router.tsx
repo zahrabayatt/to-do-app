e354761661c24b903e45e2ff1f121a844f891390
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-16 15:48:07 +0330

    Add deploy config

--- src/router.tsx
+++ src/router.tsx
@@ -4,16 +4,19 @@ import ErrorPage from "./pages/ErrorPage";
 import TasksPage from "./pages/TasksPage";
 import CategoriesPage from "./pages/CategoriesPage";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { path: ":categoryId?", element: <TasksPage /> },
-      { path: "categories", element: <CategoriesPage /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <ErrorPage />,
+      children: [
+        { path: ":categoryId?", element: <TasksPage /> },
+        { path: "categories", element: <CategoriesPage /> },
+      ],
+    },
+  ],
+  { basename: import.meta.env.BASE_URL }
+);
 
 export default router;
