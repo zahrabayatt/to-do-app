a237450ece751baa4bb52296e4a19d4824fb9d9c
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-16 16:04:25 +0330

    Fix not showing logo image

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import { cn } from "../utils";
+import logo from "../assets/logo.webp";
 
 const Logo = () => {
   return (
     <div className="flex gap-x-4 items-center">
       <Link to={"/"} className="size-8 shrink-0">
         <img
-          src="./src/assets/logo.webp"
+          src={logo}
           className="size-full cursor-pointer"
           alt="To Do App Logo"
         />
